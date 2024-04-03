4903af6fe88384777fc98b0ea05fa0e90272a5d6
Daniel Lee <danielhle922@gmail.com>
2024-04-02 22:31:00 -0400

    fix path issues

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 
-// 
 const app = express();
 app.use(cors());
 app.use(express.json());
